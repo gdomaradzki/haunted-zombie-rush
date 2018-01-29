3d114295ff08875900bb5e738a947081794f334c
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-01-29 17:23:27 -0200

    Add music and sound effects to game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     [SerializeField] private float jumpForce = 100f;
+    [SerializeField] private AudioClip sfxJump;
     private Animator anim;
     private Rigidbody rigidBody;
     private bool jump = false;
+    private AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
             anim.Play("Jump");
+            audioSource.PlayOneShot(sfxJump);
             rigidBody.useGravity = true;
             jump = true;
         }
