35e8531182e2788d4bfd7c5b56c7367b05864223
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-01-29 15:01:16 -0200

    Add behaviour to obscales

---
+++ Assets/Scripts/RockBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBehaviour : MonoBehaviour {
+    [SerializeField] Vector3 topPosition;
+    [SerializeField] Vector3 bottomPosition;
+    [SerializeField] float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(Move(bottomPosition));
+	}
+
+    IEnumerator Move (Vector3 target) {
+        while (Mathf.Abs((target - transform.localPosition).y) > 0.20f) {
+            Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+            transform.localPosition += direction * Time.deltaTime * speed;
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        Vector3 newTarget = target.y == topPosition.y ? bottomPosition : topPosition;
+
+        StartCoroutine(Move(newTarget));
+    }
+}
