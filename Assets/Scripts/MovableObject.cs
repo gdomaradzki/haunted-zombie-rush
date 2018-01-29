0a03c2ce56e939baeacdce67a891e14f90ddca3f
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-01-29 14:32:33 -0200

    Add script that moves and resets platform

---
+++ Assets/Scripts/MovableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour {
+    [SerializeField] private float objectSpeed = 1;
+    private float resetPosition = -50.8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+
+        if (transform.localPosition.x <= resetPosition) {
+            Vector3 newPosition = new Vector3(70.5f, transform.position.y, transform.position.z);
+            transform.position = newPosition;
+        }
+    }
+}
